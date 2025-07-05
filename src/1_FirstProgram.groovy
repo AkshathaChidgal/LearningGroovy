876f06eb64bd5f0c070f1db388444c6ee30269d0
AkshathaChidgal <cakshathachidgal@gmail.com>
2025-07-05 21:22:22 +0530

    Groovy Scripting

--- src/1_FirstProgram.groovy
+++ src/1_FirstProgram.groovy
@@ -1,3 +1,4 @@
 println"Hello World"
 println "Hello, Akshatha! Welcome to Groovy 🎉"
 print'first groovy script'
+print'I love groovy script'
--- src/2_Comments.groovy
+++ src/2_Comments.groovy
@@ -1,7 +1,17 @@
 //Single line comments
 print"Welcome to Groovy" //printing single line comments
-
+print'Comments are same as jave'
 print'Below is multiline comment'
 /* Multiline comments
 This is multiline comments
+ */
+
+/*
+Groovydoc Comment /*
+/** */
+
+/*
+Shebang line comments
+#! /usr/bin/env groovy
+
  */
